8cfaf694dabd15f4ef39ae4e8037010e972b02c9
Louis Pollin <louis.pollin@tv5monde.org>
2019-05-15 11:26:47 +0800

    [Page - Experience] Add page experience

---
+++ src/pages/ExperiencePage.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import '@ionic/core/css/core.css';
+import '@ionic/core/css/ionic.bundle.css';
+import {
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent, 
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonItem,
+} from '@ionic/react';
+
+const ExperiencePage = () => (
+  <>
+    <IonHeader>
+      <IonToolbar color="primary">
+        <IonTitle>Louis POLLIN - Experience</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent>
+      <IonCard>
+        <IonCardHeader>
+          <IonCardTitle>Experience</IonCardTitle>
+          <IonCardSubtitle>Software Engineer</IonCardSubtitle>
+        </IonCardHeader>
+        <IonCardContent>
+          <IonItem>
+            <p>2011 - 2014</p>
+            <h3>Internship 1 - Company 1</h3>
+            <p><em>Cupidatat sed ut tempor sed sunt consectetur duis ut incididunt. Fugiat ad eiusmod esse dolore excepteur ea aute eu quis eu commodo sed voluptate fugiat esse dolor.</em></p>
+          </IonItem>
+          <IonItem>
+            <p>2014 - 2018</p>
+            <h3>Internship 2 - Company 2</h3>
+            <p><em>Non voluptate adipisicing dolore dolor voluptate nostrud sunt consectetur laborum. Ad esse dolor fugiat laboris eu et veniam occaecat ad voluptate elit dolore irure.</em></p>
+          </IonItem>
+          <IonItem>
+            <p>2011 - 2014</p>
+            <h3>Internship 3 - Company 3</h3>
+            <p><em>Laboris ea sit irure anim occaecat ut dolor do elit minim amet dolore irure ad. Qui in laboris nulla voluptate eu in velit enim proident excepteur eiusmod ut.</em></p>
+          </IonItem>
+        </IonCardContent>
+      </IonCard>
+    </IonContent>
+  </>
+)
+
+export default ExperiencePage;
