bd1fe4a1037c4f516abe741530424082421dc933
Louis Pollin <louis.pollin@tv5monde.org>
2019-05-16 11:41:08 +0800

    [Design - Training] New design implemented
    Scroll not working so did not test dynamic design

--- src/pages/TrainingPage.tsx
+++ src/pages/TrainingPage.tsx
@@ -12,6 +12,7 @@ import {
   IonTitle,
   IonToolbar,
   IonItem,
+  IonList,
 } from '@ionic/react';
 import './TrainingPage.css'
 
@@ -23,32 +24,35 @@ const TrainingPage = () => (
       </IonToolbar>
     </IonHeader>
     <IonContent>
-
+      
       <div className="timeline">
-        <div className="container left">
-          <div className="content">
-            <h2>2011 - 2014</h2>
-            <h4>Scientific Baccalaureate</h4>
-            <p>Lycee Portes de l'Oisans - Vizille, France</p>
-            <p><em>obtained with honors</em></p>
+          <div className="timeline-item active">
+              <div className="year">2018 - Now <span className="marker"><span className="dot"></span></span>
+              </div>
+              <div className="info">
+                <IonCard class="info-card">
+                  That song the artist formerly known as prince sang no longer applies.
+                </IonCard>
+              </div>
           </div>
-        </div>
-        <div className="container right">
-          <div className="content">
-            <h2>2014 - 2018</h2>
-            <h4>BACHELOR, MASTER IN COMPUTER ENGINEERING</h4>
-            <p>Lycee Portes de l'Oisans - Vizille, France</p>
-            <p><em>obtained with honors</em></p>
+          <div className="timeline-item">
+              <div className="year">2014 - 2018 <span className="marker"><span className="dot"></span></span>
+              </div>
+              <div className="info">
+                <IonCard class="info-card">
+                  The in-house Gawker chat room is filled with photos of Rob Ford, and for one reason: Rob Ford takes a good goddamn photo.
+                </IonCard>
+              </div>
           </div>
-        </div>
-        <div className="container left">
-          <div className="content">
-            <h2>2011 - 2014</h2>
-            <h4>SCIENTIFIC BACCALAUREATE</h4>
-            <p>Lycee Portes de l'Oisans - Vizille, France</p>
-            <p><em>obtained with honors</em></p>
+          <div className="timeline-item">
+              <div className="year">2011 - 2014 <span className="marker"><span className="dot"></span></span>
+              </div>
+              <div className="info">
+                <IonCard class="info-card">
+                  That song the artist formerly known as prince sang no longer applies.
+                </IonCard>
+               </div>
           </div>
-        </div>
       </div>
 
     </IonContent>
